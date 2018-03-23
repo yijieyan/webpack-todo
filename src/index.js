5c774b9cf3ce0da3250f8ee6badd8bffdf27a0b6
yijie.yan <yijie.yan@outlook.com>
2018-03-23 09:24:50 +0800

    modify index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import App from './app.vue'
 import VueRouter from 'vue-router'
 import router from './router/index.js'
 Vue.use(VueRouter)
-import './assert/style/test.css'
-import './assert/style/test.styl'
-let root = document.getElementById("app")
-
-document.body.appendChild(root)
 
 new Vue({
   router: router(),
   render: (h) => h(App)
-}).$mount(root)
+}).$mount(app)
